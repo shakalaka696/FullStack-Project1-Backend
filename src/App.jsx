53ac0e509fa45164845a43c6ff8daabe7f2d43cf
Gavin Pereira <gavinpereira93080@gmail.com>
2026-03-10 12:30:16 +0530

    Structured components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
 
 function App() {
  
   return (
+    <>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/login" element={<div>Login page</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+     
     <h1>Hello world</h1>
+</>
   )
 }
 
